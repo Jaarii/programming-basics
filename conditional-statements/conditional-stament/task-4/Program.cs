619033d84cadaa4fc92256c768cc7ac003157216
Jaarii <1501479@co.saimia.fi>
2017-09-22 15:26:05 +0300

    project

--- conditional-statements/conditional-stament/task-4/Program.cs
+++ conditional-statements/conditional-stament/task-4/Program.cs
@@ -10,6 +10,16 @@ namespace task_4
     {
         static void Main(string[] args)
         {
+            // Define instructions
+            Console.WriteLine("Ohjelma selvittää on syötetty luku pariton tai parillinen.");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            // Define variables
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
         }
     }
 }
