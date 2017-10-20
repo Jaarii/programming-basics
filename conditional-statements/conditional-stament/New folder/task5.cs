fe254f1b6c7ced2449b485ecbfc474c3a74f86e4
jaarii <1501479@co.saimia.fi>
2017-10-20 20:43:27 +0300

    task-5-almost-ready

--- conditional-statements/conditional-stament/New folder/task5.cs
+++ conditional-statements/conditional-stament/New folder/task5.cs
@@ -8,7 +8,7 @@ namespace task_5
         {
             Console.WriteLine("Tervetuloa");
             Console.Write("Syötä ikäsi: ");
-            double ticetPrice = 100;
+            double ticetPrice = 16;
             double discount = 0;
             string userInput = Console.ReadLine();
             int age = int.Parse(userInput);
@@ -16,40 +16,72 @@ namespace task_5
             if (age < 7)
             {
                 Console.WriteLine("Alle 7v ilmaiseksi");
-            }
-            else if (age >= 65)
-            {
-                Console.WriteLine("65c ja yli saavat 50% alennuksen");
-            }
-            else if (age >= 7 && age < 15)
-            {
-                Console.WriteLine("Lipun hinta 16€");
-            }
-            else 
-            {
-                Console.Write("Oletko MTK:n jäsen [K/E]: ");
-                string isMTK = Console.ReadLine();
-                
-                if(isMTK.ToUpper()=="K")
                 {
-                    discount = 0.15;
+                    Console.WriteLine("Olet alle 7v [K/E]");
+                    string isteen = Console.ReadLine();
+
+                    if (isteen.ToUpper() == "K")
+                    {
+                        discount = 1.0;
+                    }
+
+                    else if (age >= 65)
+                    {
+                        Console.WriteLine("Olet yli 65v [K/E]");
+                        string isold = Console.ReadLine();
+
+                        if (isold.ToUpper() == "K")
+                        {
+                            discount = 0.5;
+                        }
+                    }
+                    else if (age >= 7 && age < 15)
+                    {
+                        Console.WriteLine("Lipun hinta 16€");
+
+                    }
+                    else
+                    {
+
+                        Console.Write("Oletko MTK:n jäsen [K/E]: ");
+                        string isMTK = Console.ReadLine();
+
+                        if (isMTK.ToUpper() == "K")
+                        {
+                            discount = 0.15;
+                        }
+                        else
+
+                            Console.Write("Oletko opiskelija [K/E]");
+                        string isMTKopiskelija = Console.ReadLine();
+
+                        if (isMTKopiskelija.ToUpper() == "K") ;
+                        {
+                            discount = 0.6;
+                        }
+
+                        Console.Write("Oletko varusmies [K/E]: ");
+                        string isSoldier = Console.ReadLine();
+                        if (isSoldier.ToUpper() == "K")
+                        {
+                            discount = 0.5;
+                        }
+
+                        Console.Write("Oletko opiskelija [K/E]: ");
+                        string isstudent = Console.ReadLine();
+                        if (isstudent.ToUpper() == "K")
+                        {
+                            discount = 0.45;
+                        }
+
+
+                    }
+
+
+                    Console.WriteLine($"Lipun hinta sinulle on: {ticetPrice - ticetPrice * discount}");
+                    Console.ReadKey();
+
                 }
-
-                Console.Write("Oletko varusmies [K/E]: ");
-                string isSoldier = Console.ReadLine();
-                if (isSoldier.ToUpper() == "K")
-                {
-                    discount = 0.5;
-                }
-
-
             }
-
-
-            Console.WriteLine($"Lipun hinta sinulle on: {ticetPrice - ticetPrice * discount}");
-            Console.ReadKey();
-
-        }
-    }
-}
+        
 
