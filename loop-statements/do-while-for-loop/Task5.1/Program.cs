99fdc64967bf10a6cc73e410e5ee01cb658ddf82
Jaarii <1501479@co.saimia.fi>
2017-10-23 14:05:03 +0300

    Do-While-For-Loop tasks initial and done

---
+++ loop-statements/do-while-for-loop/Task5.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä luku myös neg. luvut kelpaavat: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int k = 1; // Factor which could be 1 or -1
+
+            int even = 0;
+            int odd = 0;
+
+            if (number < 0)
+            {
+                k = -1;
+            }
+
+            for (int i = 0; i <= number * k; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even = even + i;
+                }
+
+                else
+                {
+                    odd = odd + i;
+                }
+            }
+
+
+
+            Console.WriteLine($"parittomien summa: {odd*k} ja parillisten summa: {even*k}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loop-statements/do-while-for-loop/do-while.for-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while.for_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Syötä luku: ");
+                string userInput = Console.ReadLine();
+                int luku = int.Parse(userInput);
+
+                int i = 1;
+                int f = 0;
+                int tulos = 1;
+
+                for (i = 1; i <= luku; i++) // => i = i + 1;
+                {
+                    f = f + i;
+                    tulos = f;
+                }
+
+                Console.WriteLine($"syötit: {luku} != {tulos}");
+                Console.ReadKey();
+
+
+
+
+            
+        }
+    }
+}
