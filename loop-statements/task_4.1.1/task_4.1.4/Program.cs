039c94be694b3ed7ac114edbab24457f9ca97524
Jaarii <1501479@co.saimia.fi>
2017-10-24 14:26:50 +0300

    looptask_4.1->

---
+++ loop-statements/task_4.1.1/task_4.1.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_4._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Anna heittojen lukumäärä : ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            int klaavat = 0;
+            int kruunat = 0;
+
+            for(int i = 0; i < userInput; i++)
+            {
+                int kolikko = rnd.Next(2);
+
+                if(kolikko == 0)
+                {
+                    klaavat++;
+                }
+                else
+                {
+                    kruunat++;
+                }
+            }
+            Console.WriteLine($"*Kruunuja tuli {kruunat} Klaavoja tuli {klaavat}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loop-statements/task_4.1.1/task_4.1.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_4._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for(int i=0;i<13;i++)
+            {
+                double rndValue = rnd.NextDouble();
+                if (rndValue < 0.4)
+                    Console.WriteLine($"{i+1}. 1\t Kotivoitto\a ");
+                else if(rndValue >0.4 && rndValue <0.6)
+                    Console.WriteLine($"{i + 1}. X\t Tasapeli");
+                else
+                    Console.WriteLine($"{i + 1}. 2\t Vierasvoitto ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
