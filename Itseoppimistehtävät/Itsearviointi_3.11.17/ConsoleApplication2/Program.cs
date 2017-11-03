b7c333b5203a5f7bb3e927b41fd631501cd16bbd
Jaarii <1501479@co.saimia.fi>
2017-11-03 14:33:52 +0200

    itsearviointi_task

---
+++ Itseoppimistehtävät/Itsearviointi_3.11.17/ConsoleApplication2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä lukuja");
+            string s = Console.ReadLine();
+            int number = int.Parse(s);
+            int i = 0;
+
+            do { 
+                if (number == -1)
+                {
+                    break;
+                }
+            else
+                {
+                    i = i + number;
+                }
+                             
+                s = Console.ReadLine();
+                number = int.Parse(s);
+        }
+        while(true);
+
+        Console.WriteLine($"Lukujen summa on{i}");
+            Console.ReadKey();
+
+           /* if (s > 0)
+            {
+                Console.WriteLine("Syötä lisää lukuja");
+            }
+                
+            else (s < 0)
+            {
+                Console.WriteLine("Syötit negatiivisen numeron");
+            }*/
+            
+
+
+
+        }
+    }
+}
