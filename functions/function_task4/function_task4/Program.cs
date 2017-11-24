254886cef6c6bb0b45612442a4175b4edecf30a6
Jaarii <1501479@co.saimia.fi>
2017-11-24 10:24:38 +0200

    function-task4-valmis

--- functions/function_task4/function_task4/Program.cs
+++ functions/function_task4/function_task4/Program.cs
@@ -10,123 +10,45 @@ namespace function_task4
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Syötä seuraavaksi 10 lukua");
-
-            Console.Write("Syötä luku 1/10: ");
-            string userInput = Console.ReadLine();
-            int x1 = int.Parse(userInput);
-            if (x1 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 2/10: ");
-            userInput = Console.ReadLine();
-            int x2 = int.Parse(userInput);
-            if (x2 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 3/10: ");
-            userInput = Console.ReadLine();
-            int x3 = int.Parse(userInput);
-            if (x3 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 4/10: ");
-            userInput = Console.ReadLine();
-            int x4 = int.Parse(userInput);
-            if (x4 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 5/10: ");
-            userInput = Console.ReadLine();
-            int x5 = int.Parse(userInput);
-            if (x5 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 6/10: ");
-            userInput = Console.ReadLine();
-            int x6 = int.Parse(userInput);
-            if (x6 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 7/10: ");
-            userInput = Console.ReadLine();
-            int x7 = int.Parse(userInput);
-            if (x7 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 8/10: ");
-            userInput = Console.ReadLine();
-            int x8 = int.Parse(userInput);
-            if (x8 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 9/10: ");
-            userInput = Console.ReadLine();
-            int x9 = int.Parse(userInput);
-            if (x9 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            Console.Write("Syötä luku 10/10: ");
-            userInput = Console.ReadLine();
-            int x10 = int.Parse(userInput);
-            if (x10 < 0)
-            {
-                Console.WriteLine("Luku ei ole hyväksyttävä");
-
-            }
-
-            string getNfind = getNfind;
-            Console.WriteLine(find);
+            int maxIndex = 0;
+            Console.WriteLine($"Suurin luku oli {Numerot(ref maxIndex)} ja se oli {maxIndex}.");
+            //Console.WriteLine($"Suurin luku oli {Numerot()}");
             Console.ReadKey();
         }
-        public static string getNfind(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10)
+        static int Numerot(ref int maxIndex)
+        //static int Numerot()
         {
+            int num = 0;
+            int max = 0;
 
-            string res;
+            bool isNumber;
+            string output = "";
+            Console.WriteLine("Syötä 10 positiivista lukua");
+
+            //for (int i = 0; i < 10; i++)
+            int i = 0;
+            while (i < 10)
             {
-
-                res = FindMax(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10);
-
-                Console.WriteLine(res);
-                Console.ReadKey();
+                Console.Write($"{i + 1}. ");
+                isNumber = int.TryParse(Console.ReadLine(), out num);
+                if (isNumber && num > 0)
                 {
-                    return res;
+                    output += $"{num} "; // luodaan palaute esim. 5 10 7 22 6 2 5 10
+                    if (max < num)
+                    {
+                        max = num;
+                        maxIndex = i + 1;
+                    }
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte, syötä positiivinen luku");
+                    // i--;
                 }
             }
-        }
-
-        private static string FindMax(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10)
-        {
-            throw new NotImplementedException();
+            Console.WriteLine($"Syötit seuraavat luvut:\n{output}\n");
+            return max;
         }
     }
-
 }
